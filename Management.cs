bdec2362fcb7304dfeec5d5da7e900b73dcff1c4
Anisha0906 <anishadas880@gmail.com>
2022-10-22 19:12:21 +0530

    [Anisha]Add: Product Review Management using LINQ and Transactions

---
+++ Management.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    class Management
+    {
+       
+        public readonly DataTable dataTable = new DataTable();
+        public void TopRecords(List<ProductReview> listProductReview)
+        {
+            
+            var recordedData = (from productReviews in listProductReview
+                                orderby productReviews.Rating descending
+                                select productReviews).Take(3);
+          
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+                Console.WriteLine("-----------------------------------------------------------------");
+            }
+        }
+        public void SelectedRecords(List<ProductReview> listProductReview)
+        {
+          
+
+            var recordedData = from productReviews in listProductReview
+                               where (productReviews.ProductID == 1 || productReviews.ProductID == 4 || productReviews.ProductID == 9)
+                               && productReviews.Rating > 3
+                               select productReviews;
+            Console.WriteLine("Rating greater than 3 with product id of 1,4,or 9: ");
+           
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+                Console.WriteLine("-----------------------------------------------------------------");
+            }
+        }
+        public void RetrieveCountOfRecords(List<ProductReview> listProductReview)
+        {
+           
+            var recordedData = listProductReview.GroupBy(x => x.ProductID).Select(x => new { ProductID = x.Key, Count = x.Count() });
+            Console.WriteLine("Count of records by ProductID: ");
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ID:" + list.ProductID + "----->" + "Count: " + list.Count);
+
+            }
+        }
+        public void RetrieveProductIDAndReview(List<ProductReview> listProductReview)
+        {
+            var recordedData = listProductReview.Select(x => new { ProductID = x.ProductID, Review = x.Review });
+            Console.WriteLine("ID with Review");
+            foreach (var list in recordedData)
+            {
+               
+                Console.WriteLine("ID:" + list.ProductID + "=====>" + list.Review);
+
+            }
+        }
+       
+        public void SkipTopFiveRecords(List<ProductReview> listProductReview)
+        {
+
+            var recordedData = (from productReviews in listProductReview
+                                select productReviews).Skip(5);
+            Console.WriteLine("Retrieving records after skipping top 5 records- ");
+            foreach (var list in recordedData)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+
+            }
+        }
+        public void SelectProductIDAndReviews(List<ProductReview> listProductReview)
+        {
+           
+            var recordedData = listProductReview.Select(x => new { x.ProductID, x.Review });
+            foreach (var list in recordedData)
+            {
+                
+                Console.WriteLine("ID:" + list.ProductID + "=====>" + list.Review);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Product Review Management Program.");
+           
+            List<ProductReview> productReviewList = new List<ProductReview>()
+            {
+               
+                new ProductReview(){ProductID=1,UserID=1,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=3,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=4,UserID=1,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=5,UserID=1,Rating=2,Review="nice",isLike=true},
+                new ProductReview(){ProductID=6,UserID=1,Rating=1,Review="bas",isLike=true},
+                new ProductReview(){ProductID=8,UserID=1,Rating=1,Review="Good",isLike=false},
+                new ProductReview(){ProductID=8,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProductID=2,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProductID=10,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProductID=11,UserID=1,Rating=3,Review="nice",isLike=true},
+                new ProductReview(){ProductID=12,UserID=1,Rating=2,Review="Good",isLike=true},
+                new ProductReview(){ProductID=12,UserID=1,Rating=4,Review="Good",isLike=true},
+                new ProductReview(){ProductID=13,UserID=1,Rating=5,Review="Good",isLike=true},
+                new ProductReview(){ProductID=14,UserID=1,Rating=6,Review="Good",isLike=false},
+                new ProductReview(){ProductID=15,UserID=1,Rating=2,Review="nice",isLike=true},
+                new ProductReview(){ProductID=15,UserID=1,Rating=1,Review="bad",isLike=true},
+                new ProductReview(){ProductID=15,UserID=1,Rating=2,Review="bad",isLike=true},
+                new ProductReview(){ProductID=15,UserID=1,Rating=1,Review="Good",isLike=false},
+                new ProductReview(){ProductID=18,UserID=1,Rating=9,Review="nice",isLike=true},
+                new ProductReview(){ProductID=19,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProductID=20,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProductID=21,UserID=1,Rating=3,Review="nice",isLike=true},
+                new ProductReview(){ProductID=22,UserID=1,Rating=10,Review="nice",isLike=true},
+                new ProductReview(){ProductID=22,UserID=1,Rating=8,Review="nice",isLike=true},
+                new ProductReview(){ProductID=25,UserID=1,Rating=3,Review="nice",isLike=true}
+
+            };
+           
+            foreach (var list in productReviewList)
+            {
+                Console.WriteLine("ProductID:- " + list.ProductID + " " + "UserID:- " + list.UserID
+                    + " " + "Rating:- " + list.Rating + " " + "Review:- " + list.Review + " " + "isLike:- " + list.isLike);
+                Console.WriteLine("-----------------------------------------------------------------");
+            }
+            Console.WriteLine("UC2- Retrieving Top 3 records based on rating: ");
+            Management management = new Management();
+            management.TopRecords(productReviewList);
+            Console.WriteLine("\n");
+            Console.WriteLine("UC3- Retrieving records with specific conditions: ");
+            management.SelectedRecords(productReviewList);
+            Console.WriteLine("UC4- Count: ");
+            management.RetrieveCountOfRecords(productReviewList);
+            Console.WriteLine("UC5- Retrieving review by Product ID:");
+            management.RetrieveProductIDAndReview(productReviewList);
+            Console.WriteLine("UC6- Skipping top 5 records:");
+            management.SkipTopFiveRecords(productReviewList);
+            Console.WriteLine("UC7- Retrieving review by Product ID:");
+            management.SelectProductIDAndReviews(productReviewList);
+            Console.WriteLine("UC7- Create DataTable .");
+            DataTableOperations dataTableOperations = new DataTableOperations();
+            dataTableOperations.CreateDataTable();
+            Console.ReadLine();
+
+        }
+    }
+}
